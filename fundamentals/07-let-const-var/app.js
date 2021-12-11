fde96e185a49001b0b507fc7e3215bf5ff361fc9
Jericho <jerichobantiquete@gmail.com>
2021-12-11 20:02:54 +0800

    let const and var

---
+++ fundamentals/07-let-const-var/app.js
@@ -0,0 +1,22 @@
+// let vs. const vs. var
+
+// using var
+var age = 21;
+age = 22;
+
+// using let
+let firstName = "jericho";
+firstName = "Jericho";
+
+// using const or constant -> can't reassign a value
+// always declare a value in const
+
+const lastName = "bantiquete";
+// lastName = "Bantiquete"; // TypeError: Assignment to constant variable.
+
+// console.logs
+console.log(age);
+console.log(firstName);
+console.log(lastName);
+
+// let and const comes in es6 (2015)
