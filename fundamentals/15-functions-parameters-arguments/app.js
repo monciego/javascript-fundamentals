52d12f6d6f9d01f08cc12d4b7c1ac194ba33c117
Jericho <jerichobantiquete@gmail.com>
2021-12-12 01:21:49 +0800

    functions parameters and arguments

---
+++ fundamentals/15-functions-parameters-arguments/app.js
@@ -0,0 +1,17 @@
+// parameters - when declare/define
+// placeholders, local variables
+// arguments - when invoke/call/run
+// use variables/values, multiple parameters, undefined
+
+// declaring a variable
+function hello(person) {
+  // your code goes here...
+  console.log(`Hello ${person}`);
+}
+
+// greet harry
+hello("Harry");
+// greet ron
+hello("Ron");
+// greet hermione
+hello("Hermione");
