5862af8007455100471985dde9bdcdf736d15bd4
Jericho <jerichobantiquete@gmail.com>
2021-12-11 19:40:11 +0800

    variable naming rules

---
+++ fundamentals/05-variable-naming-rules/app.js
@@ -0,0 +1,19 @@
+// can contain digits(numbers), letters, $
+// must start with letter, $ or _
+
+// no keyword
+// cannot start with number
+
+// case sensitive - fullname vs Fullname
+
+// camelCase or underscore?
+
+let random123_$ = "Hello JavaScript";
+console.log(random123_$);
+
+// firstname and Firstname is not the same
+let firstname = "Dinesh";
+console.log(firstname);
+
+let Firstname = "Gilfoyle";
+console.log(Firstname);
