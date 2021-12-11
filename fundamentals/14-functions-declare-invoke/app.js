a825dd6a3291552c080022ce8ea68f6f3abdf8f9
Jericho <jerichobantiquete@gmail.com>
2021-12-12 01:06:21 +0800

    functions declare and invoke

---
+++ fundamentals/14-functions-declare-invoke/app.js
@@ -0,0 +1,14 @@
+// Functions - a set of statements that performs a task
+// Functions are one of the fundamental building blocks in JavaScript.
+
+// Functions - declare, invoke
+
+// declaring a variable
+function hello() {
+  // your code goes here...
+  console.log("Hello Harry");
+  console.log("Hello Ron");
+  console.log("Hello Hermione");
+}
+
+hello(); // invoking a variable
