0ebd0a85f9534153b8a32c4fdb4d29d891989fb1
Jericho <jerichobantiquete@gmail.com>
2021-12-11 20:42:23 +0800

    number basics

---
+++ fundamentals/09-number-basics/app.js
@@ -0,0 +1,27 @@
+// numbers
+// loosely typed = don't declare type
+// not needed to specify what type of number (int, float)
+
+const pi = 3.14;
+let number = 08;
+
+let random = 312;
+random = "now it's a string";
+
+console.log(pi);
+console.log(number);
+console.log(random);
+
+// math operations
+const num1 = 5;
+const num2 = 23;
+
+const add = num1 + num2;
+const subtract = num1 - num2;
+const multiply = num1 * num2;
+const division = num1 / num2;
+
+console.log(add); // 28
+console.log(subtract); // -18
+console.log(multiply); // 115
+console.log(division); // 0.21739130434782608
