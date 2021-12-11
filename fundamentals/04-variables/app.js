42a77843182926b621824ded39f22f1db8b2f484
Jericho <jerichobantiquete@gmail.com>
2021-12-11 19:31:26 +0800

    variables

---
+++ fundamentals/04-variables/app.js
@@ -0,0 +1,17 @@
+// variable - most basic building block
+// variable - store, access, modify ==== value
+// declare, assignment operator, assign value
+
+let myName = "Jericho Bantiquete";
+
+let address, age, gender;
+
+address = "Philippines";
+age = 21;
+gender = "Male";
+// myName = "Jericho";
+
+console.log(myName);
+console.log(address);
+console.log(age);
+console.log(gender);
