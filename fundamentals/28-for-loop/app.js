27ca7cc29e64e0ccb270e9d65bdfcd66bb4625a7
Jericho <jerichobantiquete@gmail.com>
2021-12-16 02:32:19 +0800

    for loop

---
+++ fundamentals/28-for-loop/app.js
@@ -0,0 +1,6 @@
+// for loop - loops through a block of code a number of times
+// A for loop repeats until a specified condition evaluates to false.
+
+for (let i = 0; i < 10; i++) {
+  console.log(`The number is ${i}`);
+}
