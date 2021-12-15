6a7fdab99119ec3c7cf8a0f4d68095d5180b949a
Jericho <jerichobantiquete@gmail.com>
2021-12-16 00:27:37 +0800

    while loop

---
+++ fundamentals/26-while-loop/app.js
@@ -0,0 +1,9 @@
+// while loop - while loop loops through a block of code as long as a specified condition is true.
+// turn off auto save to avoid conflict (endless loop)
+
+let amount = 10;
+
+while (amount > 0) {
+  console.log(`I have ${amount} dollars`);
+  amount--;
+}
