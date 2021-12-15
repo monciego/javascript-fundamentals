946c58b90e8ded64c0571f2cd578670cc5a570e8
Jericho <jerichobantiquete@gmail.com>
2021-12-15 21:06:31 +0800

    conditional statements basic

---
+++ fundamentals/21-conditional-statements/app.js
@@ -0,0 +1,21 @@
+// conditional statements
+// >, <, >=, <=, ==, ===, !=, !===
+
+// checks if it is true or false
+
+if (true) {
+  console.log("Hello World");
+}
+
+const value = 2 < 1;
+if (value) {
+  console.log(value); // it won't print
+}
+
+const num = 21;
+
+if (num >= 18) {
+  console.log("the number is greater than or equals to 18"); // this will print of the condition is true
+} else {
+  console.log("the number is less than 18"); // this will print if the condition is false
+}
