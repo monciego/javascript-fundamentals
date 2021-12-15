e2b341ff7e09b97a6aed215a2ba1962de99f32e5
Jericho <jerichobantiquete@gmail.com>
2021-12-16 02:26:56 +0800

    do while loop

---
+++ fundamentals/27-do-while-loop/app.js
@@ -0,0 +1,11 @@
+// do while
+// The do...while statement creates a loop that executes a specified statement until the test condition evaluates to false.
+// runs at least
+// while loop will not run if not met the condition, do while will run once
+
+let money = 0;
+
+do {
+  console.log(`I have ${money} pesos`);
+  money++;
+} while (money < 10);
