0a76b7a076d07322907f5cf38cd630baf408be3d
Jericho <jerichobantiquete@gmail.com>
2021-12-15 23:53:16 +0800

    equality

---
+++ fundamentals/23-equality/app.js
@@ -0,0 +1,18 @@
+// equality
+//  == checks only value
+// === checks value and type
+
+const num1 = "6";
+const num2 = 6;
+
+const value = num1 == num2; // true
+const value2 = num1 === num2; // false
+
+// not equal
+const value3 = num1 != num2; // false
+const value4 = num1 !== num2; // true
+
+console.log(value);
+console.log(value2);
+console.log(value3);
+console.log(value4);
