f202494f38f0408d2c8fc61dae5db428fad89562
Jericho <jerichobantiquete@gmail.com>
2021-12-16 00:18:50 +0800

    switch statements

---
+++ fundamentals/25-switch-statement/app.js
@@ -0,0 +1,27 @@
+// switch = The switch statement is used to perform different actions based on different conditions.
+// dice values: 1 - 6
+
+const dice = 5;
+
+switch (dice) {
+  case 1:
+    console.log("You got one");
+    break;
+  case 2:
+    console.log("You got two");
+    break;
+  case 3:
+    console.log("You got three");
+    break;
+  case 4:
+    console.log("You got four");
+    break;
+  case 5:
+    console.log("You got five");
+    break;
+  case 6:
+    console.log("You got six");
+    break;
+  default:
+    console.log("you did not roll the dice");
+}
