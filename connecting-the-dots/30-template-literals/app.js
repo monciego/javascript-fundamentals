1b84783fecdfe3541c2de5a656e84305bcb3568b
Jericho <jerichobantiquete@gmail.com>
2021-12-16 03:10:41 +0800

    template literals

---
+++ connecting-the-dots/30-template-literals/app.js
@@ -0,0 +1,11 @@
+// template literals - es6
+// backtick characters `` - above tab (left from one)
+// interpolation ${} - insert expression(value)
+
+const firstName = "Jericho";
+const age = 21;
+
+const greeting = `Hello my name is ${firstName}, i'm ${age} years old. And here some simple math 10 + 50 is equal to ${
+  10 + 50
+}`;
+console.log(greeting); // Hello my name is Jericho, i'm 21 years old. And here some simple math 10 + 50 is equal to 60
