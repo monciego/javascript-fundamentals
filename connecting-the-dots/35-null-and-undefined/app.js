857fadbb6a08fcbe1fa33c0d52f671ae7c1051db
Jericho <jerichobantiquete@gmail.com>
2021-12-16 19:35:02 +0800

    null and undefined

---
+++ connecting-the-dots/35-null-and-undefined/app.js
@@ -0,0 +1,16 @@
+// null and undefined
+// both represent "no value"
+
+// undefined - "javascript can't find a valeu for this"
+
+// variable without value
+// missing functions arguments
+// missing object properties
+
+// null - "developer sets the value"
+
+let number = 20 + null; // 20 + 0
+console.log(number); // 20
+
+let number2 = 20 + undefined;
+console.log(number2); // NaN - not a number
