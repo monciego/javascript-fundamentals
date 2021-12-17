3daeeeff0af9e356735e31655a44e783bb757407
Jericho <jerichobantiquete@gmail.com>
2021-12-17 22:44:02 +0800

    document.getElementById

---
+++ dom/52-getElementById/app.js
@@ -0,0 +1,9 @@
+// select the element or group of element we want
+// decide the effect we want to apply to the selection
+
+// getElementById("element");
+
+const h1 = document.getElementById("title");
+h1.style.color = "blue";
+
+document.getElementById("btn").style.backgroundColor = "green";
