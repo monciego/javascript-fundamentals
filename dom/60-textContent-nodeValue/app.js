1d906681a0ac2eaa11371f62c5e4c73d0cba6519
Jericho <jerichobantiquete@gmail.com>
2021-12-18 12:21:57 +0800

    textContent and nodeValue

---
+++ dom/60-textContent-nodeValue/app.js
@@ -0,0 +1,9 @@
+// nodeValue
+
+const item = document.getElementById("special");
+const value = item.firstChild.nodeValue;
+console.log(value);
+
+// textContent
+const value1 = item.textContent;
+console.log(value1);
