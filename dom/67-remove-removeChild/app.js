0de57578b48b8c516ca070491d931592567b4963
Jericho <jerichobantiquete@gmail.com>
2021-12-18 16:14:14 +0800

    Remove And RemoveChild

---
+++ dom/67-remove-removeChild/app.js
@@ -0,0 +1,9 @@
+// remove
+// removeChild
+
+const result = document.getElementById("result");
+const h1child = result.querySelector("h1");
+result.removeChild(h1child);
+
+const h1 = document.querySelector("h1");
+h1.remove();
